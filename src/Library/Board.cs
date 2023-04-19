628ad58346c99b11b8fd75f158f9458989cd310f
TadeoKi <tadeo.ki@correo.ucu.edu.uy>
2023-04-19 18:10:08 -0300

    Progreso1

---
+++ src/Library/Board.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PII_Game_Of_Life
+{
+    public class Board
+    {
+        public static void Tablero()
+        {
+            for (int  y=0; y<ReadFile.contentLines.Length;y++)
+            {
+                for (int x=0; x<ReadFile.contentLines[y].Length; x++)
+                {
+                    if(ReadFile.contentLines[y][x]=='1')
+                    {
+                        ReadFile.board[x,y]=true;
+                    }
+                }
+            }
+        }
+    }
+}
